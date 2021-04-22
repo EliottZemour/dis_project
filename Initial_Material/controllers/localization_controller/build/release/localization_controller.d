02654599c8929e8bcda2e994d3546d1769fd1099
nicolasmarbot <82210983+nicolasmarbot@users.noreply.github.com>
2021-04-22 18:19:30 +0200

    Delete Initial_Material directory

--- Initial_Material/controllers/localization_controller/build/release/localization_controller.d
+++
@@ -1,9 +0,0 @@
-build/release/localization_controller.o: localization_controller.c \
- C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
- C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
- C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/accelerometer.h \
- C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/position_sensor.h \
- trajectories.h odometry.h
