4ae0c524292dc80a97c7b3af5a564f1ac708b1e6
nicolasmarbot <82210983+nicolasmarbot@users.noreply.github.com>
2021-04-29 12:53:28 +0200

    odometry acc qui marche

--- Initial_Material/controllers/localization_controller/odometry.c
+++ Initial_Material/controllers/localization_controller/odometry.c
@@ -5,7 +5,7 @@
 
 #include "odometry.h"
 
-//-----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------//
 /*CONSTANTES*/
 #define WHEEL_AXIS 		0.057 		// Distance between the two wheels in meter
 #define WHEEL_RADIUS 	0.020		// Radius of the wheel in meter
@@ -27,29 +27,37 @@ static pose_t _odo_pose_acc, _odo_speed_acc, _odo_pose_enc;
  * @param      odo       The odometry
  * @param[in]  acc       The acceleration
  * @param[in]  acc_mean  The acc mean
+ 
+ LE odo_compute_acc fonctionne mais il faut encore initialiser la position
  */
 void odo_compute_acc(pose_t* odo, const double acc[3], const double acc_mean[3])
 {
+	double a = _odo_pose_enc.heading;
+	
 	// To Do : Compute the acceleration in frame A + remove biais (Assume 1-D motion)
 	double acc_wx = (acc[1] - acc_mean[1]);
+	double acc_wy = -(acc[0] - acc_mean[0]);
            // printf("mean acc : %g %g %g\n",acc_mean[0], acc_mean[1] , acc_mean[2]);
 	// To Do : Implement your motion model (Assume 1-D motion)
+	double norm_acc= sqrt(pow(acc[1],2.0)+pow(acc_wy,2.0));
+	double speed= norm_acc * _T;
 	
-	//printf("value of _T : %g", _T);
-	_odo_speed_acc.x += acc_wx *_T;
-	_odo_pose_acc.x += _odo_speed_acc.x * _T;
-	
-	double acc_wy = -(acc[0] - acc_mean[0]);
-	_odo_speed_acc.y += acc_wy *_T;
-	_odo_pose_acc.y += _odo_speed_acc.y * _T;
+	double speed_wx = speed * cos(a);
 
+            double speed_wy = speed * sin(a);
+	_odo_pose_acc.x += speed_wx * _T ;
+	_odo_pose_acc.y += speed_wy * _T;
+            
+            
+           _odo_pose_acc.heading = a ;
+            
 	memcpy(odo, &_odo_pose_acc, sizeof(pose_t));
 	
-	if(VERBOSE_ODO_ACC)
-            {
+	
  		printf("ODO with acceleration : %g %g %g\n", odo->x , odo->y , RAD2DEG(odo->heading));
-     	 }	
+     	 	
 }
+
 
 /**
  * @brief      Compute the odometry using the encoders
@@ -57,6 +65,7 @@ void odo_compute_acc(pose_t* odo, const double acc[3], const double acc_mean[3])
  * @param      odo         The odometry
  * @param[in]  Aleft_enc   The delta left encoder
  * @param[in]  Aright_enc  The delta right encoder
+ LE odo_compute_acc fonctionne mais il faut encore initialiser la position
  */
 void odo_compute_encoders(pose_t* odo, double Aleft_enc, double Aright_enc)
 {
