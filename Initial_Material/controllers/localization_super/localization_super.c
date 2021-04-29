89ea2eefd64e4a939dba1f4a66f4d6a9846c88e6
nicolasmarbot <82210983+nicolasmarbot@users.noreply.github.com>
2021-04-29 15:13:23 +0200

    performance metrics with bugs

--- Initial_Material/controllers/localization_super/localization_super.c
+++ Initial_Material/controllers/localization_super/localization_super.c
@@ -4,20 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
+#define RAD2DEG(X)      X / M_PI * 180.0
 #include <webots/robot.h>
 #include <webots/emitter.h>
 #include <webots/supervisor.h>
 
+
 #define FLOCK_SIZE	1		// Number of robots in flock
 #define TIME_STEP	64		// [ms] Length of time step
 
-WbNodeRef robs[FLOCK_SIZE];		// Robots nodes
-WbFieldRef robs_trans[FLOCK_SIZE];	// Robots translation fields
-WbFieldRef robs_rotation[FLOCK_SIZE];	// Robots rotation fields
+WbNodeRef robs[1];		// Robots nodes
+WbFieldRef robs_trans[1];	// Robots translation fields
+WbFieldRef robs_rotation[1];	// Robots rotation fields
 
 
-float loc[FLOCK_SIZE][3];
+float loc[3];
 #define RULE1_THRESHOLD 0.2		// Location of everybody in the flock
 static FILE *fp;
 
@@ -38,40 +39,20 @@ void reset(void) {
 	wb_robot_init();
 
 	char rob[7] = "ROBOT1";
-	int i;
-	for (i=0;i<FLOCK_SIZE;i++) {
+
 		sprintf(rob,"ROBOT%d", 1);
-		robs[i] = wb_supervisor_node_get_from_def(rob);
-		robs_trans[i] = wb_supervisor_node_get_field(robs[i],"translation");
-		robs_rotation[i] = wb_supervisor_node_get_field(robs[i],"rotation");
-	}
+		robs[0] = wb_supervisor_node_get_from_def(rob);
+		robs_trans[0] = wb_supervisor_node_get_field(robs[0],"translation");
+		robs_rotation[0] = wb_supervisor_node_get_field(robs[0],"rotation");
+	
 }
-
-
-// void send_init_poses(void) {
-  	// char buffer[255];	// Buffer for sending data
-	// int i;
-         
-	// for (i=0;i<FLOCK_SIZE;i++) {
-		//Get data
-		// loc[i][0] = wb_supervisor_field_get_sf_vec3f(robs_trans[i])[0]; // X
-		// loc[i][1] = wb_supervisor_field_get_sf_vec3f(robs_trans[i])[2]; // Z
-		// loc[i][2] = wb_supervisor_field_get_sf_rotation(robs_rotation[i])[3]; // THETA
-
-		//Send it out
-		// sprintf(buffer,"%1d#%f#%f#%f",i+offset,loc[i][0],loc[i][1],loc[i][2]);
-
-		//Run one step
-		// wb_robot_step(TIME_STEP);
-	// }
-// }
 
 void controller_init_log(const char* filename)
 {
 
   fp = fopen(filename,"w");
   
-   //fprintf(fp, "time; loc_x; loc_z_; theta\n");
+   fprintf(fp, "time; loc_x; loc_z_; theta\n");
   
 
 
@@ -83,42 +64,46 @@ void controller_print_log(double time)
   if( fp != NULL)
   {
     fprintf(fp, "%g;  %g; %g; %g\n",
-            time, loc[0][0],loc[0][1], loc[0][2]);
+            time, loc[0],loc[1], RAD2DEG(loc[2]));
   
 
 }
 }
 
+/**
+void compute_fitness_acc(float* fit) {
+
+
+	*fit =abs(loc[0]-_odo_acc.x)+abs(loc[1]-_odo_acc.y);
+	}
+		
+*/
 
 
 /*
  * Main function.
  */
- 
+ 	float fit_acc =0;
 int main() 
 {	// Buffer for sending data
-	int i;			// Index
 	
 
             
-			controller_init_log("pose.csv");
+	controller_init_log("pose.csv");
 	reset();
 	for(;;) {
-		wb_robot_step(TIME_STEP);
+          	     wb_robot_step(TIME_STEP);
 		
 		if (t % 10 == 0) {
-			for (i=0;i<FLOCK_SIZE;i++) {
 
-				// Get data
-				loc[i][0] = wb_supervisor_field_get_sf_vec3f(robs_trans[i])[0]; // X
-				loc[i][1] = wb_supervisor_field_get_sf_vec3f(robs_trans[i])[2]; // Z
-				loc[i][2] = wb_supervisor_field_get_sf_rotation(robs_rotation[i])[3]; // THETA
-				
-				// Sending positions to the robots, comment the following two lines if you don't want the supervisor sending it                   		
-				//sprintf(buffer,"%1d#%f#%f#%f##%f#%f",i+offset,loc[i][0],loc[i][1],loc[i][2], migrx, migrz);
-				//wb_emitter_send(emitter,buffer,strlen(buffer));
-				controller_print_log(t);				
-			}
+			// Get data
+			loc[0] = wb_supervisor_field_get_sf_vec3f(robs_trans[0])[0]; // X
+			loc[1] = wb_supervisor_field_get_sf_vec3f(robs_trans[0])[2]; // Z
+			loc[2] = wb_supervisor_field_get_sf_rotation(robs_rotation[0])[3]; // THETA
+			
+			controller_print_log(t/1000.0);				
+			//compute_fitness_acc(&fit_acc);
+			printf("performance metric : %g\n", fit_acc);
 			
 		}
 
