c6f32184e6f8cb804df8b752cdbdafeea91052e5
nicolasmarbot <82210983+nicolasmarbot@users.noreply.github.com>
2021-05-12 15:57:24 +0200

    follower/leader/formation_super ajoutés

---
+++ Initial_Material/controllers/leader/build/release/leader.d
@@ -0,0 +1,9 @@
+build/release/leader.o: leader.c \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/differential_wheels.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\nicol\AppData\Local\Programs\Webots/include/controller/c/webots/emitter.h
