3a01d06342a581d56439232ed04b06d5183d7e7d
nicolasmarbot <82210983+nicolasmarbot@users.noreply.github.com>
2021-06-06 13:58:43 +0200

    formation super commenté

--- Initial_Material/controllers/formation_super/formation_super.c
+++ Initial_Material/controllers/formation_super/formation_super.c
@@ -28,9 +28,10 @@ static WbFieldRef robs_translation[ROBOTS];
 static WbFieldRef robs_rotation[ROBOTS];
 WbDeviceTag emitter_device;
 
-static FILE *fp;
 
-float loc[ROBOTS][3];
+static FILE *fp;    // File in which metric data is written for further plotting
+
+float loc[ROBOTS][3];   // pose of each robot
 
 int t;
 
@@ -41,7 +42,7 @@ float rel_z[ROBOTS-1];
 float center[2] = {0.0, 0.0};
 float center_old[2] = {0.0, 0.0};
 
-float d_max = 6.28 * 0.0205 * 0.064;
+float d_max = 6.28 * 0.0205 * 0.064; // Maximal speed for the speed metric computation
 
 /* Good relative positions for each robot */
 double good_rp[ROBOTS][2] = { {0.0,0.0}, {0.1,0.0}, {-1.0,0.0}, {0.2,0.0}, {-0.2,0.0} };
@@ -67,6 +68,8 @@ void reset(void) {
 	emitter_device = wb_robot_get_device(emitter0);
 }
 
+
+// Computation of the distance component of the metric
 void compute_dist_metric() {
 	dfo_metric = 0.0;
 	int i;
@@ -77,7 +80,7 @@ void compute_dist_metric() {
 	dfo_metric = 1.0 / dfo_metric;
 }
 
-
+// Computation of the velocity component of the metric
 void compute_veloc_metric() {
     int i;
     for (i=0; i< ROBOTS; i++) {
@@ -92,6 +95,7 @@ void compute_veloc_metric() {
     v_metric /= d_max;
 }
 
+// Creation and initialization of formation_metric.csv in which data is written
 void controller_init_log(const char* filename)
 {
 
@@ -104,7 +108,7 @@ void controller_init_log(const char* filename)
 }
 
 
-// printing of data in the log file "flocking_metric.csv"
+// printing of data in the log file "formation_metric.csv"
 void controller_print_log(double time)
 {
 
@@ -124,17 +128,19 @@ int main(int argc, char *args[]) {
 		print_enabled = atoi(args[1]);
 		printf("Print: %d\n", print_enabled);
 	}
-	controller_init_log("formation_metric.csv");
+	
+	controller_init_log("formation_metric.csv"); // initialization of goal log file
+	
 	reset();
 
 	for(;;) { 
 		wb_robot_step(TIME_STEP); /* run one step */
 		
-		//if (t % 10 == 0) {
+
 			center_old[0] = center[0];
 			center_old[1] = center[1];
 			for (i=1;i<ROBOTS;i++) {
-				/* Get data */
+				/* Get pose data for each robot*/
 				loc[0][0] = wb_supervisor_field_get_sf_vec3f(robs_translation[0])[0];
 				loc[0][1] = wb_supervisor_field_get_sf_vec3f(robs_translation[0])[2];
 				loc[0][2] = wb_supervisor_field_get_sf_rotation(robs_rotation[0])[3];
@@ -151,17 +157,19 @@ int main(int argc, char *args[]) {
 				rel_z[i-1] = global_x*sinf(loc[i][2]) + global_z*cosf(loc[i][2]);
 
 			}
-
+                                 // Intermediate metric computation
 			compute_dist_metric();
 			compute_veloc_metric();
 			printf("dfo metric: %.2f\n",dfo_metric);
 			printf("veloc metric: %.2f\n",v_metric);
+			
+			// Final metric computation
 			fit_formation = dfo_metric * v_metric;
 
 			if (print_enabled)
 				printf("Performance metric: %.2f\n",fit_formation);
-		//}
-		controller_print_log(t/1000.0);
+		
+		controller_print_log(t/1000.0); //writing of data in the log file with related time t
 		t += TIME_STEP;	
 	}
 }
